600d583fa8aaf8e94390657e4c449dc7cc6e64cd
Pedro Gomes <pedro.oliveiragti@gmail.com>
2024-02-08 20:14:52 -0300

    Added Appoints in the same Date

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,15 +1,31 @@
 import { v4 as uuidv4 } from "uuid";
 import { Router } from "express";
-import { startOfHour, parseISO } from "date-fns";
+import { startOfHour, parseISO, isEqual } from "date-fns";
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post("/", (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find((appointment) =>
+    isEqual(parsedDate, appointment.date)
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: "This Appointment is already booked" });
+  }
 
   const appointment = {
     id: uuidv4(),
