e54d2846d3e9d78898057025c9740a859afd61f2
Pedro Gomes <pedro.oliveiragti@gmail.com>
2024-02-08 23:43:34 -0300

    Errors

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -14,17 +14,24 @@ appointmentsRouter.get("/", (request, response) => {
 
 //Post
 appointmentsRouter.post("/", (request, response) => {
-  const { provider, date } = request.body;
+  try {
+    const { provider, date } = request.body;
 
-  const parsedDate = parseISO(date);
+    const parsedDate = parseISO(date);
 
-  const createAppointment = new CreateAppointmentServices(
-    appointmentsRepository
-  );
+    const createAppointment = new CreateAppointmentServices(
+      appointmentsRepository
+    );
 
-  const appointment = createAppointment.execute({ date: parsedDate, provider });
+    const appointment = createAppointment.execute({
+      date: parsedDate,
+      provider,
+    });
 
-  return response.json(appointment);
+    return response.json(appointment);
+  } catch (err: any) {
+    return response.status(400).json({ error: err.message });
+  }
 });
 
 export default appointmentsRouter;
