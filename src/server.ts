3db916295d46391fb0e0b30b77427357edfbaf3a
Pedro Gomes <pedro.oliveiragti@gmail.com>
2024-02-12 02:00:51 -0300

    Fixing some mistakes ☺☺

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
-
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello Word" });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log("Server Started on port 3333! 😉");
