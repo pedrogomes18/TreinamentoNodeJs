e5c67e7df50c261d7d6433b8cf1e52b736cbf413
Pedro Gomes <pedro.oliveiragti@gmail.com>
2024-02-13 17:51:42 -0300

    Docs API - Use Live Server no index.html para funcionar

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import path from "path";
 import routes from "./routes";
 
 const app = express();
+app.use(
+  "/docs",
+  express.static(path.join(__dirname, "..", "..", "..", "..", "docs"))
+);
 app.use(express.json());
 app.use(routes);
 
